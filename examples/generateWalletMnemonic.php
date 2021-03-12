29ac88af626d5f4342c45659d0f77fcfb7f25844
highbreedtech <support@highbreedtech.com>
2021-03-12 02:28:58 +0100

    fix excess comments

--- examples/generateWalletMnemonic.php
+++ examples/generateWalletMnemonic.php
@@ -3,9 +3,6 @@ require("../src/Tatum.php");
 
 $Tatum = new Tatum\Tatum();
 $mnemonic = 'allow symbol play float page buddy bus impose february sister junk mouse torch improve dad grape industry regular kick student scout off vocal year';
-//$mnemonic = 'lift tomato broccoli fade mercy subject fog genuine issue crouch fly usage monkey debris uphold eager common matter syrup wood glance ship imitate inmate';
-//$mnemonic = 'pretty erase host arrive depart pioneer else palm thunder away mind job toast mirror chuckle banner balcony color vital someone unfair attract learn blossom';
-$xpub = "";
 $coin = "BCH";
 $wallet = $Tatum->generateWallet($coin, $mnemonic); 
 echo $wallet;
